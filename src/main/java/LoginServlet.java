58b4abb012dcb108954c7e517e517d8e74bef8a5
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-07-19 12:07:15 -0400

    feat: cookies and session ex

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,6 +8,12 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("user") != null ){
+            response.sendRedirect("/profile");
+            return;
+        }
+
+
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
@@ -17,6 +23,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
